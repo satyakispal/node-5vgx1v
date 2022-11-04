f3840ef181ebc25bd3a9e1a24dc88405dc2b0272
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-04 13:50:07 +0530

    1 file modified

--- equals1.js
+++ equals1.js
@@ -9,7 +9,7 @@ if (a == b) {
 if (a === b) {
   console.log('a and b both are of same value and type..');
 } else {
-  console.log('a and b both are of different value and type or both..');
+  console.log('a and b both are of different value or type or both..');
 }
 
 if (b == c) {
@@ -20,7 +20,7 @@ if (b == c) {
 if (b === c) {
   console.log('b and c both are of same value and type..');
 } else {
-  console.log('b and c both are of different value and type or both..');
+  console.log('b and c both are of different value or type or both..');
 }
 
 if (a == c) {
@@ -31,5 +31,5 @@ if (a == c) {
 if (a === c) {
   console.log('a and c both are of same value and type..');
 } else {
-  console.log('a and c both are of different value and type or both..');
+  console.log('a and c both are of different value or type or both..');
 }
