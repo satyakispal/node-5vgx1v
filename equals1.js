427ef7b1009f6efb66f5d14d803319ca3239a740
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-04 13:46:19 +0530

    1 file added

---
+++ equals1.js
@@ -0,0 +1,13 @@
+var a = 2;
+let b = 2;
+var c = 7;
+if (a == b) {
+  console.log('a and b both are of same value only..');
+} else {
+  console.log('a and b both are of different value only..');
+}
+if (a === b) {
+  console.log('a and b both are of same value and type..');
+} else {
+  console.log('a and b both are of different value and type or both..');
+}
