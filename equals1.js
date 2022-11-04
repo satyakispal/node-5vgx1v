22ed6c605008c380c9673ce9a8fd53f4a6def6a1
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-04 13:48:22 +0530

    1 file modified

--- equals1.js
+++ equals1.js
@@ -11,3 +11,25 @@ if (a === b) {
 } else {
   console.log('a and b both are of different value and type or both..');
 }
+
+if (b == c) {
+  console.log('b and c both are of same value only..');
+} else {
+  console.log('b and c both are of different value only..');
+}
+if (b === c) {
+  console.log('b and c both are of same value and type..');
+} else {
+  console.log('b and c both are of different value and type or both..');
+}
+
+if (a == c) {
+  console.log('a and c both are of same value only..');
+} else {
+  console.log('a and c both are of different value only..');
+}
+if (a === c) {
+  console.log('a and c both are of same value and type..');
+} else {
+  console.log('a and c both are of different value and type or both..');
+}
