fdd63055e3dab0f869d285b3884025d5bb3242d0
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-04 13:39:55 +0530

    1 file modified

--- hoisting4.js
+++ hoisting4.js
@@ -1,5 +1,6 @@
 home();
-var home = function () {
- console.log('hello'); // ReferenceError: Cannot access 'home' before initialization (let home)
- // home is not a function (var home)
+var home = function () // or let home = function ()
+{
+  console.log('hello'); // ReferenceError: Cannot access 'home' before initialization (let home)
+  // home is not a function (var home)
 };
