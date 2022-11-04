a9cd5ce90aa6c4bfdbd854a416e6457a88de460e
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-04 10:53:14 +0530

    1 file modified

--- new1.js
+++ new1.js
@@ -1,44 +1,45 @@
 'use strict';
 
+//1
 // let a = 074;
-let b = '\056';
-
+// let b = '\056';
 // console.log(a);
-console.log(b);
-// // SyntaxError: Octal literals are not allowed in strict mode
+// SyntaxError: Octal literals are not allowed in strict mode
+// console.log(b);
+// SyntaxError: Octal escape sequences are not allowed in strict mode
 
 // let personal = { firstname: 'Satyaki' };
-// // delete personal;
-// // // SyntaxError: Delete of an unqualified identifier in strict mode
+// delete personal;
+// SyntaxError: Delete of an unqualified identifier in strict mode
 // delete personal.firstname;
-// // correct
+// correct
 // console.log(personal);
 // **********************************************
 // b = 'hey';
 // console.log(b);
-// // ReferenceError: b is not defined
+// ReferenceError: b is not defined
 
 // 5
 // details = { name: 'satyaki', age: 30 };
 // console.log(details);
-// // ReferenceError: details is not defined
+// ReferenceError: details is not defined
 
 // 5
 // let object2 = {};
 // Object.defineProperty(object2, 'b', { value: 29, writable: false });
-// // assignment to a non-writable property
+// assignment to a non-writable property
 // object2.b = 9;
 // console.log(object2.b);
-// // TypeError: Cannot assign to read only property 'b' of object '#<Object>'
+// TypeError: Cannot assign to read only property 'b' of object '#<Object>'
 
 // 6
 // a = NaN;
 // console.log(a);
-// // ReferenceError: a is not defined
+// ReferenceError: a is not defined
 
 // 7
 // let arguments = 'hey';
 // let eval = 47;
 // console.log(arguments);
 // console.log(eval);
-// // SyntaxError: Unexpected eval or arguments in strict mode
+// SyntaxError: Unexpected eval or arguments in strict mode
