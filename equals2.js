4732150a936210aeceec82b370bb94d6563ea6fb
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-04 14:06:32 +0530

    1 file modified

--- equals2.js
+++ equals2.js
@@ -5,55 +5,30 @@ var d = { a: 2, b: 7 };
 var e = [{ k: 11 }, { p: 16 }];
 var f = ['str1', 'str2'];
 
-if (a == b) {
-  console.log('a and b both are of same value only..');
-} else {
-  console.log('a and b both are of different value only..');
-}
 if (a === b) {
   console.log('a and b both are of same value and type..');
 } else {
   console.log('a and b both are of different value or type or both..');
 }
 
-if (a == c) {
-  console.log('a and c both are of same value only..');
-} else {
-  console.log('a and c both are of different value only..');
-}
 if (a === c) {
   console.log('a and c both are of same value and type..');
 } else {
   console.log('a and c both are of different value or type or both..');
 }
 
-if (a == d) {
-  console.log('a and d both are of same value only..');
-} else {
-  console.log('a and d both are of different value only..');
-}
 if (a === d) {
   console.log('a and d both are of same value and type..');
 } else {
   console.log('a and d both are of different value or type or both..');
 }
 
-if (a == e) {
-  console.log('a and e both are of same value only..');
-} else {
-  console.log('a and e both are of different value only..');
-}
 if (a === e) {
   console.log('a and e both are of same value and type..');
 } else {
   console.log('a and e both are of different value or type or both..');
 }
 
-if (a == f) {
-  console.log('a and f both are of same value only..');
-} else {
-  console.log('a and f both are of different value only..');
-}
 if (a === f) {
   console.log('a and f both are of same value and type..');
 } else {
